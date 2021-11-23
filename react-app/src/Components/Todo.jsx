a37470df8d06cfafc500a559252f07631fc2bd6b
KoPoine <tmdrbwk3@gmail.com>
2021-11-23 12:57:14 +0900

    11-23
    react-todoList

--- react-app/src/Components/Todo.jsx
+++ react-app/src/Components/Todo.jsx
@@ -1,27 +1,72 @@
-import { useState } from "react";
-import styled from "styled-components";
+import { useState, useRef } from "react";
+import styled, { css } from "styled-components";
 
 const TodoCompocent = () => {
-  const [add, setAdd] = useState("");
-  const btnAdd = () => {
-    setAdd(add);
+  const inputRef = useRef(null);
+  const [nextId, setnextId] = useState(3);
+  const [text, setText] = useState("");
+  const [arr, setArr] = useState([
+    {
+      id: 1,
+      text: "할일1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      text: "할일2",
+      isDone: false,
+    },
+  ]);
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleClick = (e) => {
+    setArr([...arr, { id: nextId, text, isDone: false }]);
+    setText("");
+    setnextId(nextId + 1);
+    inputRef.current.focus();
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleClick();
+    }
+  };
+  const handleDelete = (id) => {
+    const newArr = arr.filter((todo) => todo.id !== id);
+    setArr(newArr);
+  };
+  const handleToggle = (e, id) => {
+    const isDone = e.target.checked;
+    const newArray = arr.map((todo) => {
+      return todo.id === id ? { ...todo, isDone } : todo;
+    });
+    console.log(newArray);
+    setArr(newArray);
   };
   return (
     <>
       <Container>
-        <Title>일정관리</Title>
+        <Title>ToDo List</Title>
         <InputWrapper>
-          <Input placeholder="할일 입력" />
-          <BtnAdd onClick={setAdd}>등록</BtnAdd>
+          <Input
+            onKeyPress={handleKeyPress}
+            ref={inputRef}
+            placeholder="할일 입력"
+            onChange={handleChange}
+            value={text}
+          />
+          <BtnAdd onClick={handleClick}>등록</BtnAdd>
         </InputWrapper>
         <TodoList>
-          <TodoItem onChange={btnAdd}>
-            <label>
-              <input type="checkbox" />
-              <TodoContent>{add}</TodoContent>
-            </label>
-            <BtnDelete>삭제</BtnDelete>
-          </TodoItem>
+          {arr.map(({ id, text, isDone }) => (
+            <TodoItem key={id} isDone={isDone}>
+              <label>
+                <input type="checkbox" onChange={(e) => handleToggle(e, id)} />
+                <TodoContent isDone={isDone}>{text}</TodoContent>
+              </label>
+              <BtnDelete onClick={() => handleDelete(id)}>삭제</BtnDelete>
+            </TodoItem>
+          ))}
         </TodoList>
       </Container>
     </>
@@ -37,7 +82,7 @@ const Title = styled.div`
   padding: 20px;
   text-align: center;
   font-weight: 800;
-  font-size: 16px;
+  font-size: 26px;
   background: #82b6d8;
 `;
 const InputWrapper = styled.div`
@@ -63,6 +108,33 @@ const BtnAdd = styled.button`
   background: #332e2e;
   color: white;
 `;
+
+const TodoList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+const TodoContent = styled.span``;
+
+const TodoItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
+  padding: 15px;
+  border-bottom: 1px solid #332e2e;
+  border-radius: 6px;
+
+  ${({ isDone }) =>
+    isDone &&
+    css`
+      background: #332e2e;
+      color: #aca5a5;
+      ${TodoContent} {
+        padding: 10px;
+        font-size: 18px;
+        text-decoration: "line-through";
+      }
+    `}
+`;
 const BtnDelete = styled.button`
   cursor: pointer;
   border: none;
@@ -70,20 +142,4 @@ const BtnDelete = styled.button`
   background: #332e2e;
   color: white;
 `;
-const TodoList = styled.ul`
-  list-style: none;
-
-  padding: 0;
-`;
-const TodoItem = styled.li`
-  display: flex;
-  justify-content: space-between;
-  margin: 30px 10px;
-  border-bottom: 1px solid #332e2e;
-`;
-const TodoContent = styled.span`
-  padding: 10px;
-  width: 100%;
-`;
-
 export default TodoCompocent;
