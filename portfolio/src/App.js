b3c87b74316a205c13f78b6324ceda3b43283260
NaiDDa <77822025+NaiDDa@users.noreply.github.com>
2021-12-14 19:46:15 +0900

    +
    +

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -12,13 +12,14 @@ function App() {
             <Item>PROJECTS</Item>
             <Item>CONTACT</Item>
           </Menu>
-          <TopText>
-            <H4>안녕하세요, 김승환입니다.</H4>
-            <H4>저는 신입 프론트엔드 개발자 입니다.</H4>
-            <H4>저의 포트폴리오 페이지에 와주셔서 감사합니다.</H4>
-          </TopText>
+          <Box>
+            <TopText>
+              <H4>안녕하세요, 김승환입니다.</H4>
+              <H4>저는 신입 프론트엔드 개발자 입니다.</H4>
+              <H4>저의 포트폴리오 페이지에 와주셔서 감사합니다.</H4>
+            </TopText>
+          </Box>
         </TopMenu>
-
         <HeaderImg src="img/code.png" />
       </Header>
     </Wrapper>
@@ -52,12 +53,14 @@ const Menu = styled.ul`
   display: flex;
   justify-content: flex-end;
 `;
-const TopText = styled.div`
-  justify-content: center;
-  margin-top: 80px;
+const Box = styled.div`
   display: flex;
-  align-items: center;
-  flex-direction: column;
+  justify-content: center;
+`;
+const TopText = styled.div`
+  text-align: center;
+  width: 600px;
+  margin-top: 80px;
   color: #ddd;
   border-bottom: 3px solid #ddd;
   border-top: 3px solid #ddd;
